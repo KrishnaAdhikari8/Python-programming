a8d60e1fd70a7082243fad79258d2fe3be7cc41e
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-05-25 15:14:05 +0545

    qno17

--- Questions/qno17.py
+++ Questions/qno17.py
@@ -1,1 +1,8 @@
 # Check whether a string is palindrome
+
+string = input("enter the string to cheak the palindrome or not ? ")
+rev_string = string[::-1]
+if string == rev_string:
+    print("pallindrome")
+else:
+    print("not pallindrome")
