d1917f9818074781b99290a93109dd244a8b5862
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-06-25 20:15:24 +0545

    generator

---
+++ 13,generator.py
@@ -0,0 +1,16 @@
+# An iterator is an object that can be iterated (looped) through one element at a time using __iter__() and __next__() methods.
+class Count:
+    def __init__(self, max):
+        self.max = max
+        self.num = 1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.num <= self.max:
+            value = self.num
+            self.num += 1
+            return value
+        raise StopIteration
+c = Count(5)
+for i in c:
+    print(i)
