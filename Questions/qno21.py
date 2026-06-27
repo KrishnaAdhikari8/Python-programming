0bca4a63ff060c3478930f3361d5efe368d72361
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-06-27 11:06:34 +0545

    qno21

---
+++ Questions/qno21.py
@@ -0,0 +1,7 @@
+# Find largest and smallest number in a list
+
+l=[1,2,6,9,4,7,8,0,3]
+largest = max(l)
+smallest = min(l)
+print("largest number in list :",largest)
+print("smallest number in list :",smallest)
